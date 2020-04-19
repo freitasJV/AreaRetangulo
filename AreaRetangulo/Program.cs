fd8dc4f1cef7471c4235fb6056a7298023f9436f
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 14:27:16 -0300

    Add project files.

---
+++ AreaRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Dados do retângulo");
+            Console.Write("Largura: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(ret);
+
+
+        }
+    }
+}
---
+++ AreaRetangulo/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace AreaRetangulo
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * Largura + 2 * Altura;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+        public override string ToString()
+        {
+            return $"ÁREA = {Area().ToString("F2", CultureInfo.InvariantCulture)}\nPERÍMETRO = {Perimetro().ToString("F2",CultureInfo.InvariantCulture)}\nDIAGONAL = {Diagonal().ToString("F2",CultureInfo.InvariantCulture)}";
+        }
+    }
+}
