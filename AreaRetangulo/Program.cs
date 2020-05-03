4c2b0b9bdb7988c8a532f910133ef36559cb932c
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 16:19:10 -0300

    Adicionando tratamento de excessões

--- AreaRetangulo/Program.cs
+++ AreaRetangulo/Program.cs
@@ -7,17 +7,26 @@ namespace AreaRetangulo
     {
         static void Main(string[] args)
         {
-            Retangulo ret = new Retangulo();
+            try
+            {
+                Retangulo ret = new Retangulo();
 
-            Console.WriteLine("Dados do retângulo");
-            Console.Write("Largura: ");
-            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.Write("Altura: ");
-            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Dados do retângulo");
+                Console.Write("Largura: ");
+                ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Altura: ");
+                ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine(ret);
-
-
+                Console.WriteLine(ret);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
+            }
         }
     }
 }
